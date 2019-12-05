e009409003b9dc04397f453991ae90f715820b9f
teitumx <kirik241@ya.ru>
2019-12-05 11:04:18 +0300

    Добавил домашнюю работу по Уроку #3

---
+++ Lesson 3/hw1.js
@@ -0,0 +1,32 @@
+// // ЗАДАНИЕ #1
+
+	let numbers = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97];
+
+	var i = 0;
+
+	while (i < numbers.length) {
+	  console.log(numbers[i++]);
+	}
+
+
+// ЗАДАНИЕ #2
+
+	var basket = [
+		['пальто', 200],
+		['шуба', 400],
+		['куртка', 300]
+	];
+
+	function countBasketPrice() {
+		var sum = basket[0][1]+basket[1][1]+basket[2][1];
+		return console.log(sum);
+		
+	}
+
+	countBasketPrice()
+
+// ЗАДАНИЕ #3
+
+	for (var i = 0 ; i<=9 ; console.log(i++)){
+
+	};
